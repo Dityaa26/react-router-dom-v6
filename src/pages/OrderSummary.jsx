c137fa7280d6e044776239d0484aef901fb33970
Dityaa26 <aditya261862@gmail.com>
2023-11-02 20:42:12 +0530

    Going back to previous page

--- src/pages/OrderSummary.jsx
+++ src/pages/OrderSummary.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const OrderSummary = () => {
+    const navigate = useNavigate()
   return (
-    <div className="w-screen h-[calc(100vh-92px)] flex pt-36 justify-center">
-      <h1 className='text-4xl text-green-600 font-bold'>Order Confirm!</h1>
+    <div className="flex flex-col pt-36 justify-center items-center">
+      <h1 className="text-4xl text-green-600 font-bold">Order Confirm!</h1>
+      <button
+        type="submit"
+        className="bg-sky-400 mx-4 mt-4 p-2 font-semibold text-lg w-1/12"
+        onClick={() => navigate(-1)}
+      >
+        Go Back
+      </button>
     </div>
   );
 }
