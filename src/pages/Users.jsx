e1dd588430ccb9d08a99f2f16481366a540d9337
Dityaa26 <aditya261862@gmail.com>
2023-11-03 09:11:07 +0530

    search params

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
 import { userList } from '../utils/constants';
 
 
 const Users = () => {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const showActiveUsers = searchParams.get('filter') === 'active';
+    console.log(showActiveUsers)
+    
   return (
-    <div className='flex flex-col'>
+    <div className="flex flex-col">
+      <div className="w-2/6 rounded-full mx-auto flex justify-around bg-slate-200 mt-4">
+        <button onClick={()=> setSearchParams({filter: 'active'})} className="bg-[#fffbfb] m-2 p-2 px-4 rounded-full font-bold">
+          Active User
+        </button>
+        <button onClick={()=> setSearchParams({})} className="bg-[#fffbfb] m-2 p-2 px-4 rounded-full font-bold">
+          Reset Filter
+        </button>
+      </div>
       {userList.map((user) => (
-        <Link key={user.id} to={`/users/${user.id}`} className="text-xl mx-2 my-4 text-center rounded-md font-bold w-1/12 bg-sky-400">User: {user.name}</Link>
+        <Link
+          key={user.id}
+          to={`/users/${user.id}`}
+          className="text-xl mx-2 my-4 text-center rounded-md font-bold w-1/12 bg-sky-400"
+        >
+          User: {user.name}
+        </Link>
       ))}
     </div>
   );
